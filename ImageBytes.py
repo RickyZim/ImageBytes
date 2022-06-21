5099296ed0d26d919b58b07430ec51db21487cb0
Kematian <101077705+RickyZim@users.noreply.github.com>
2022-06-21 19:20:37 -0300

    Update ImageBytes.py

--- ImageBytes.py
+++ ImageBytes.py
@@ -1,31 +1,17 @@
-from io import BytesIO
-from PIL import Image
-from easy_pil import Editor
 from requests import get
-
+from easy_pil import Editor
+from PIL import Image
+from io import BytesIO
 
 class ImageBytes():
-    def image_to_bytes(path:str):
-        try:
-            img = Editor(path)
-            return img.image_bytes
-        except ValueError as erro:
-            return erro
-    def bytes_to_image(bytes, save = True, show = True):
-        try:
-            img = Image.open(BytesIO(bytes.read()))
-            if save == True:
-                img.save("Final.png")
-            if show == True:
-                img.show()
-            else:
-                return img.getdata
-        except ValueError as erro:
-            return erro
     def url_to_bytes(url:str):
-        try:
-            getimg = get(url)
-            byte = BytesIO(getimg.content)
-            return byte
-        except ValueError as erro:
-            return erro
+        base = get(url)
+        bytes = BytesIO(base.content)
+        return bytes
+    def bytes_to_image(bytes):
+        b = BytesIO(bytes.read())
+        img = Editor(b)
+        return img.image_bytes
+    def image_to_bytes(path:str):
+        img = Editor(path)
+        return img.image_bytes
